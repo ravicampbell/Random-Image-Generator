9bbcda4aa91b79f3ac4017566f30b3b09053e06a
ravicampbell <ravi.j.campbell@gmail.com>
2020-10-11 19:32:44 -0500

    Add files via upload

---
+++ random_image_generator.py
@@ -0,0 +1,46 @@
+import json
+import numpy as np
+import random
+import requests
+from PIL import Image
+
+
+def create_image(n):
+	RED_KEY = "5cb7ca05-e870-42f1-a480-290c1a2826e5"
+	BLUE_KEY = "f231735e-3e03-42d5-afdd-0ec0160c6c0c"
+	GREEN_KEY = "8145a4e6-45a5-4274-85a8-cbdd5c8d56b0"
+	res = np.zeros((n,n), dtype=object)
+	KEYS = [RED_KEY, BLUE_KEY, GREEN_KEY]
+	colors = [request_values(key, n*n) for key in KEYS]
+	for i in range(n):
+		for j in range(n):
+			res[i][j] = (colors[i+j][0], colors[i+j][1], colors[i+j][2])
+		
+	img = Image.fromarray(res, mode='RGB')
+	img.save('random.png')
+
+
+def request_values(api_key, n):
+	url = 'https://api.random.org/json-rpc/2/invoke'
+	text = {
+		"jsonrpc": "2.0",
+		"method": "generateIntegers",
+		"params": {
+			"apiKey": api_key,
+			"n": n,
+			"min": 0,
+			"max": 255,
+			"replacement": True
+		},
+		"id": 42
+	}
+	response = requests.post(url, json=text)
+	text = json.loads(response.text)
+	return text['result']['random']['data']
+		
+
+def main():
+    create_image(120)
+
+if __name__ == '__main__':
+    main()
